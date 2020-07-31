25566a43f745149683c8ab193ae95cbee8f1e985
Chris <c.lawsonnelson@gmail.com>
2020-07-31 10:47:29 -0400

    Video Select callback passed to Video Items

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,10 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -16,11 +19,19 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = async (video) => {
+    this.setState({ selectedVideo: video });
+    console.log(this.state.selectedVideo);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
